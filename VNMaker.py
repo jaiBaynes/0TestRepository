43196c485cb385dc965e43f48a59070e1158832e
unknown <jaibaynes@gmail.com>
2022-01-19 05:44:03 -0500

    added everything that needs to be initialized for the program to run

--- VNMaker.py
+++ VNMaker.py
@@ -1,4 +1,26 @@
 #makes visual novels from any inputted book
+
+# Imports all the things needed to make the program run
+import time
+import pygame
+import math 
+from math import sqrt
+import random
+from pygame.locals import*
+import os
+import pyttsx3
+os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (0,25) # makes appear consistantly in the top left corner of the screen
+
+# initialize the gane DO NOT EDIT
+pygame.init()
+readAloud = False
+autoplay = False
+screen = pygame.display.set_mode()
+xval, yval = screen.get_size();
+win = pygame.display.set_mode ((xval,int(yval)),HWSURFACE|DOUBLEBUF|RESIZABLE)
+FPS = 60
+clock = pygame.time.Clock()
+
 class VNCharacter:
     def __init__ (self,name, image, size, width, voice):
         self.name = name
